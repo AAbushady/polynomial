b4fcf36cdd06dae90b9e522d35a4a441e2932e9e
Alex Abushady <aabushady214@gmail.com>
2019-08-28 12:55:54 -0400

    Update polynomial.js
    Added explanation comment and changed x from var to let

--- polynomial.js
+++ polynomial.js
@@ -1,7 +1,10 @@
 /*
-*
+    This program will Take the polynomial:
+    x^2 - 4x + 7
+    
+    In this situation x will = 3.
 */
 
-var x = 3;
+let x = 3;
 
 console.log(`If x = 3, the polynomial x^2 - 4x +7 will equal: ${Math.pow(x, 2) - 4 * x + 7}`);
