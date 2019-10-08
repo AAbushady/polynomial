745b363b78c389516d4b7894fab674ccf516c7ee
Alex Abushady <aabushady214@gmail.com>
2019-10-08 12:14:57 -0400

    Update polynomial.js
    Refactored code to be less reliant on hard coded numbers.

--- polynomial.js
+++ polynomial.js
@@ -1,16 +1,20 @@
-/*
-    This program will Take the polynomial:
-    x^2 - 4x + 7
-    
-    In this situation x will = 3.
-*/
+/*global console*/
+/*jshint esversion:6*/
 
-// Welcome the User
-console.log("Welcome to Polynomial");
+// This program will Take the polynomial:
+//    x^2 - 4x + 7
+// In this situation x will = 3.
 
-let x = 3;
+(function () {
+    'use strict';
 
-console.log(`If x = 3, the polynomial x^2 - 4x +7 will equal: ${Math.pow(x, 2) - 4 * x + 7}`);
+    // Welcome the User
+    console.log("Welcome to Polynomial");
 
-// Thank the User
-console.log("Thank you for using Polynomial");
+    let x = 3;
+
+    console.log(`If x = ${x}, the polynomial (x^2 - 4x + 7) will equal: ${Math.pow(x, 2) - 4 * x + 7}`);
+
+    // Thank the User
+    console.log("Thank you for using Polynomial");
+})();
